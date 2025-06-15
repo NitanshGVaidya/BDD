e6b0fa9dd825f88db477ed2698e9c47101249497
NitanshGVaidya <vaidyagnitansh@gmail.com>
2025-06-16 00:57:48 +0530

    first commit

---
+++ src/test/java/features/update_seoid.feature
@@ -0,0 +1,6 @@
+Feature: Update user data
+
+  Scenario: Update seoid of a user from the file
+    Given the user data is loaded from the file
+    When I update the seoid
+    Then the seoid in the file should be new entered one
---
+++ src/test/java/stepdefinitions/UpdateSteps.java
@@ -0,0 +1,38 @@
+package stepdefinitions;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cucumber.java.en.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Scanner;
+
+import static org.junit.Assert.*;
+
+public class UpdateSteps {
+
+    private final File file = new File("src/test/resources/source.json");
+    private Map<String, Object> userData;
+    private String seoid;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Given("the user data is loaded from the file")
+    public void loadUserData() throws IOException {
+        userData = mapper.readValue(file, Map.class);
+    }
+
+    @When("I update the seoid")
+    public void updateEmail() throws IOException {
+            seoid = System.getProperty("newseoid", "default");
+            userData.put("seoid", seoid);
+            mapper.writeValue(file, userData);
+
+    }
+
+    @Then("the seoid in the file should be new entered one")
+    public void verifyEmail(String expectedSeoid) throws IOException {
+        Map<String, Object> updatedData = mapper.readValue(file, Map.class);
+        assertEquals(expectedSeoid, updatedData.get("seoid"));
+    }
+}
+
